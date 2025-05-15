b9fdeec387a87b493d7cf473af845d8255f52f94
fatlindaosdautaj <fatlindaosdautaj0@gmail.com>
2025-05-15 19:20:28 +0200

    init commit

---
+++ src/main/java/com/example/detyra3_siguria_e_te_dhenave/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.detyra3_siguria_e_te_dhenave;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.detyra3_siguria_e_te_dhenave {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.detyra3_siguria_e_te_dhenave to javafx.fxml;
+    exports com.example.detyra3_siguria_e_te_dhenave;
+}
