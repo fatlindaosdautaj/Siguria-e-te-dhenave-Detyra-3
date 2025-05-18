eb63b3017ca0013544192206c936b260c822cf5b
fionislamii <fion.islami@student.uni-pr.edu>
2025-05-18 08:09:38 +0200

    Update MessageReceiver.java

--- src/main/java/client/MessageReceiver.java
+++ src/main/java/client/MessageReceiver.java
@@ -4,15 +4,17 @@ import rsa.RSAUtils;
 
 import java.io.*;
 import java.net.Socket;
+import java.net.SocketException;
 import java.security.PrivateKey;
 
 public class MessageReceiver implements Runnable {
-    private Socket socket;
-    private PrivateKey clientPrivateKey;
+    private final Socket socket;
+    private final PrivateKey clientPrivateKey;
 
     public MessageReceiver(Socket socket, PrivateKey clientPrivateKey) {
         this.socket = socket;
         this.clientPrivateKey = clientPrivateKey;
+
     }
 
     @Override
@@ -22,13 +24,22 @@ public class MessageReceiver implements Runnable {
 
             while (true) {
                 String encryptedMessage = reader.readLine();
-                if (encryptedMessage != null) {
-                    String decrypted = RSAUtils.decrypt(encryptedMessage, clientPrivateKey);
-                    System.out.println("\n[Mesazh i pranuar] ➜ " + decrypted);
+
+                if (encryptedMessage == null) {
+                    // Socket u mbyll nga pala tjetër
+                    System.out.println("Lidhja me serverin u mbyll.");
+                    break;
                 }
+
+                String decrypted = RSAUtils.decrypt(encryptedMessage, clientPrivateKey);
+                System.out.println("\n[Mesazh i pranuar] ➜ " + decrypted);
+                System.out.print("> ");
             }
+
+        } catch (SocketException e) {
+            System.out.println("Lidhja është mbyllur. Receiver ndaloi.");
         } catch (Exception e) {
-            e.printStackTrace();
+            System.err.println("Gabim në marrjen e mesazhit: " + e.getMessage());
         }
     }
 }
